2a7f0290cc55ac3c228b38df29ae6755321a2cad
Daniel <danniel.pinson@gmail.com>
2021-10-25 09:21:23 +0200

    correction de la comparaison de liste et format de sortie
    LA comparaison n'est pas encore générique, elle ne marche que pour les list<Contact>
    Quelques tests supplémentaire pour GestionContacts ont été faits

--- CDAA/gestionnaireContact/main.cpp
+++ CDAA/gestionnaireContact/main.cpp
@@ -10,15 +10,15 @@ int REUSSI = 0; // Nombre de tests reussi
 
 using namespace std;
 
-bool compareListContact(list<Contact> &a, list<Contact> &b){
+bool compareListContact(list<Contact> a, list<Contact> b){
     if (a.size() != b.size())
         return false;
-    for (auto i : a){
-        for (auto j : b){
-            if (i == j)
-                continue;
-            return false;
-        }
+    list<Contact>::iterator it1 = a.begin();
+    list<Contact>::iterator it2 = b.begin();
+    while(it1 != a.end() && it2 != b.begin()){
+        if (*it1 == *it2)
+            continue;
+        return false;
     }
     return true;
 }
@@ -34,16 +34,17 @@ template <class C>
 void genericTestOutputFormat(string classTest, string s, C a, C b, bool verbose = false){
     if (a == b){
         if (verbose == true)
-            cout << "Test de la classe " << classTest << ": " << s << " succeeded" << endl;
+            cout << "Test de la classe " << classTest << ", " << s << " succeeded" << endl;
         REUSSI++;
         } else {
         cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-        cout << "Test " << classTest << " " << s << " failed" << endl;
+        cout << "Test " << classTest << ", " << s << ", failed" << endl;
         cout << "First member is: " << a << "\nSecond member is: " << b << endl;
         cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
         RATE++;
     }
 }
+
 template <class C>
 /**
  * @brief genericTestOutputFormat Renvoie sur std::cout si le test est reussi ou non
@@ -56,11 +57,11 @@ template <class C>
 void genericTestOutputFormat(string classTest, string s, list<C> a, list<C> b, bool verbose = false){
     if (compareListContact(a, b) == true){
         if (verbose == true)
-            cout << "Test de la classe " << classTest << ": " << s << " succeeded" << endl;
+            cout << "Test de la classe, " << classTest << ", " << s << ", succeeded" << endl;
         REUSSI++;
         } else {
         cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-        cout << "Test " << classTest << " " << s << " failed" << endl;
+        cout << "Test " << classTest << ", " << s << ", failed" << endl;
         cout << "First member is: {\n";
         for (auto i : a)
             cout << i << "\n";
@@ -85,11 +86,11 @@ void genericTestOutputFormat(string classTest, string s, list<C> a, list<C> b, b
 void genericTestOutputFormat(string classTest, string s, string a, string b, bool verbose = false){
     if (a == b){
         if (verbose == true)
-                cout << "Test de la classe " << classTest << ": " << s << " succeeded" << endl;
+                cout << "Test de la classe, " << classTest << ", " << s << " succeeded" << endl;
         REUSSI++;
     } else {
         cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-        cout << "Test " << classTest << " " << s << " failed" << endl;
+        cout << "Test de la classe, " << classTest << ", " << s << " failed" << endl;
         cout << "First member is: " << a << "\nSecond member is: " << b << endl;
         cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
         RATE++;
@@ -107,15 +108,15 @@ void testContact(){
     // Récupération de données
     string classTest = "Contact";
 
-    genericTestOutputFormat(classTest, "recuperation du nom,", p.getNom(), "Nom");
-    genericTestOutputFormat(classTest, "recuperation du prenom,", p.getPrenom(), "Prenom");
-    genericTestOutputFormat(classTest, "recuperation de l'entreprise,", p.getEntreprise(), "Entreprise");
-    genericTestOutputFormat(classTest, "recuperation du telephone,", p.getTelephone(), "06 52 48 61 34");
-    genericTestOutputFormat(classTest, "recuperation de la photo,", p.getPhoto(), "photo.jpg");
-    genericTestOutputFormat(classTest, "recuperation du mail,", p.getMail(), "mail");
+    genericTestOutputFormat(classTest, "recuperation du nom", p.getNom(), "Nom");
+    genericTestOutputFormat(classTest, "recuperation du prenom", p.getPrenom(), "Prenom");
+    genericTestOutputFormat(classTest, "recuperation de l'entreprise", p.getEntreprise(), "Entreprise");
+    genericTestOutputFormat(classTest, "recuperation du telephone", p.getTelephone(), "06 52 48 61 34");
+    genericTestOutputFormat(classTest, "recuperation de la photo", p.getPhoto(), "photo.jpg");
+    genericTestOutputFormat(classTest, "recuperation du mail", p.getMail(), "mail");
     Date now = Date();
     string nowS = to_string(now.getJour()) + "/" + to_string(now.getMois()) + "/" + to_string(now.getAnnee());
-    genericTestOutputFormat(classTest, "recuperation de la date,", p.getDateCrea(), nowS);
+    genericTestOutputFormat(classTest, "recuperation de la date", p.getDateCrea(), nowS);
     now.~Date();
 
 
@@ -126,12 +127,12 @@ void testContact(){
     p.setTelephone("06 45 79 85 31");
     p.setPhoto("photo2.jpg");
     p.setMail("mail2");
-    genericTestOutputFormat(classTest, "modification du nom,", p.getNom(), "Nom2");
-    genericTestOutputFormat(classTest, "modification du prenom,", p.getPrenom(), "Prenom2");
-    genericTestOutputFormat(classTest, "modification de l'entreprise,", p.getEntreprise(), "Entreprise2");
-    genericTestOutputFormat(classTest, "modification du telephone,", p.getTelephone(), "06 45 79 85 31");
-    genericTestOutputFormat(classTest, "modification de la photo,", p.getPhoto(), "photo2.jpg");
-    genericTestOutputFormat(classTest, "modification du mail,", p.getMail(), "mail2");
+    genericTestOutputFormat(classTest, "modification du nom", p.getNom(), "Nom2");
+    genericTestOutputFormat(classTest, "modification du prenom", p.getPrenom(), "Prenom2");
+    genericTestOutputFormat(classTest, "modification de l'entreprise", p.getEntreprise(), "Entreprise2");
+    genericTestOutputFormat(classTest, "modification du telephone", p.getTelephone(), "06 45 79 85 31");
+    genericTestOutputFormat(classTest, "modification de la photo", p.getPhoto(), "photo2.jpg");
+    genericTestOutputFormat(classTest, "modification du mail", p.getMail(), "mail2");
 }
 
 /**
@@ -147,14 +148,14 @@ void testInteraction(){
     Date now = Date();
     string nowS = to_string(now.getJour()) + "/" + to_string(now.getMois()) + "/" + to_string(now.getAnnee());
 
-    genericTestOutputFormat(classTest, "recuperation du contenu,", i.getContenu(), "rdv aujourd'hui par tél., RAS");
-    genericTestOutputFormat(classTest, "recuperation de la date,", i.getDateCreation(), nowS);
-    genericTestOutputFormat(classTest, "recuperation du contact,", *i.getContact(), p);
+    genericTestOutputFormat(classTest, "recuperation du contenu", i.getContenu(), "rdv aujourd'hui par tél., RAS");
+    genericTestOutputFormat(classTest, "recuperation de la date", i.getDateCreation(), nowS);
+    genericTestOutputFormat(classTest, "recuperation du contact", *i.getContact(), p);
     now.~Date();
 
     // Modifications de données
     i.setContenu("rdv interessant");
-    genericTestOutputFormat(classTest, "recuperation du contenu,", i.getContenu(), "rdv interessant");
+    genericTestOutputFormat(classTest, "recuperation du contenu", i.getContenu(), "rdv interessant");
 
     p.~Contact();
 }
@@ -169,19 +170,19 @@ void testTodo(){
     Todo t = Todo("Rappeler", "16/10/2021", "date", &i);
 
     // Test des getter
-    genericTestOutputFormat(classTest, "recuperation du contenu,", t.getContenu(), "Rappeler");
-    genericTestOutputFormat(classTest, "recuperation du contenu du tag,", t.getContenuTag(), "16/10/2021");
-    genericTestOutputFormat(classTest, "recuperation de l'interaction,", *t.getInteraction(), i);
-    genericTestOutputFormat(classTest, "recuperation du tag,", t.getTag(), "@date"); // C'est perturbant de dire que le tag c'est "date" et quand tu le récupère c'est "@date"
+    genericTestOutputFormat(classTest, "recuperation du contenu", t.getContenu(), "Rappeler");
+    genericTestOutputFormat(classTest, "recuperation du contenu du tag", t.getContenuTag(), "16/10/2021");
+    genericTestOutputFormat(classTest, "recuperation de l'interaction", *t.getInteraction(), i);
+    genericTestOutputFormat(classTest, "recuperation du tag", t.getTag(), "@date"); // C'est perturbant de dire que le tag c'est "date" et quand tu le récupère c'est "@date"
 
 
     // Test des setter
     t.setContenu("Rendez-vous");
     t.setContenuTag("17/10/2021");
     t.setTag("newTag");
-    genericTestOutputFormat(classTest, "modification du contenu,", t.getContenu(), "Rendez-vous");
-    genericTestOutputFormat(classTest, "modification du contenu du tag,", t.getContenuTag(), "17/10/2021");
-    genericTestOutputFormat(classTest, "modification du tag,", t.getTag(), "newTag");
+    genericTestOutputFormat(classTest, "modification du conten,", t.getContenu(), "Rendez-vous");
+    genericTestOutputFormat(classTest, "modification du contenu du tag", t.getContenuTag(), "17/10/2021");
+    genericTestOutputFormat(classTest, "modification du tag", t.getTag(), "newTag");
 }
 
 /**
@@ -199,17 +200,27 @@ void testGestionContact(){
     Date now = Date();
     string nowS = to_string(now.getJour()) + "/" + to_string(now.getMois()) + "/" + to_string(now.getAnnee());
 
-    genericTestOutputFormat(classTest, "recuperation de la liste de contact,", gc.getContacts(), {});
+    genericTestOutputFormat(classTest, "recuperation de la liste de contact", gc.getContacts(), {});
     genericTestOutputFormat(classTest, "récupération de la date de la derniere suppression", gc.getDerniereSuppr(), nowS);
-}
 
+    gc.addContact(p1);
+    genericTestOutputFormat(classTest, "ajout d'un contact dans la list", gc.getContacts(), {p1});
+
+    list<Contact> lp = {p1, p2};
+    gc.setContacts(lp);
+    genericTestOutputFormat(classTest, "remplacement de la liste par une nouvelle", gc.getContacts(), lp);
+
+    gc.supprContact(p2);
+    genericTestOutputFormat(classTest, "suppression du contact p1 de la liste", gc.getContacts(), {p1});
+    genericTestOutputFormat(classTest, "vérification de la mise à jour de la date de dernière suppression", gc.getDerniereSuppr(), nowS);
+}
 
 int main()
 {
     testContact(); // Tests de la classe contact
     testInteraction(); // Tests de la classe interaction
     testTodo(); // Tests de la classe Todo
-    testGestionContact();
+    testGestionContact(); // Tests de la classe GestionContacts
     cout << "Nombre de tests: " << RATE + REUSSI << "\nNombre de tests valides: " << REUSSI << "\nNombre de tests rate: " << RATE << endl;
     return 0;
 }
