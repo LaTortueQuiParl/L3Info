cbb50b80bf04f43e15e1104dea36a41210b7b720
LaTortueQuiParl <90192003+LaTortueQuiParl@users.noreply.github.com>
2021-10-31 13:25:27 +0100

    Update Point.cpp

--- SI/dragon/src/Point.cpp
+++ SI/dragon/src/Point.cpp
@@ -2,6 +2,38 @@
 
 Point::Point()
 {
+    this->x = 0;
+    this->y = 0;
+    this->z = 0;
 }
 
-Point::~Point(){}
+Point::~Point()
+{
+    //dtor
+}
+
+Point::Point(double newX, double newY, double newZ)
+{
+    this->x = newX;
+    this->y = newY;
+    this->z = newZ;
+}
+
+double Point::getX(){
+    return this->x;
+}
+double Point::getY(){
+    return this->y;
+}
+double Point::getZ(){
+    return this->z;
+}
+void Point::setX(double newX){
+    this->x = newX;
+}
+void Point::setY(double newY){
+    this->y = newY;
+}
+void Point::setZ(double newZ){
+    this->z = newZ;
+}
