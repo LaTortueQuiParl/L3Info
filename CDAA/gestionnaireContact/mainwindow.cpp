2e05169ac93a9c762d9c9c73fc6ceb2ab3b30cd6
Daniel <danniel.pinson@gmail.com>
2021-11-27 21:08:50 +0100

    Ajout de commentaire et activation des widgets

--- CDAA/gestionnaireContact/mainwindow.cpp
+++ CDAA/gestionnaireContact/mainwindow.cpp
@@ -3,11 +3,6 @@
 #include <QDebug>
 #include <QtSql/QSqlQuery>
 
-GestionBDD *MainWindow::getDb() const
-{
-    return db;
-}
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -23,27 +18,25 @@ MainWindow::MainWindow(QWidget *parent)
                                    << "Entreprise"
                                    << "Telephone"
                                    << "Photo"
-                                   << "Mail"
+                                   << "Mail",
+                     this->modelContact
+                     );
+    this->setupModel("Interaction",
+                     QStringList() << "Id"
+                                   << "Contenu"
+                                   << "DateCreation"
+                                   << "idContact"
+                                   << "idGestionTodo",
+                     modelInteraction
+                     );
+    this->setupModel("Todo",
+                     QStringList() << "Id"
+                                   << "Contenu"
+                                   << "Deadline"
+                                   << "idInteraction",
+                     modelTodo
                      );
     this->createUI();
-}
-
-void MainWindow::setupModel(const QString table, QStringList attributs){
-    modelContact = new QSqlTableModel(this);
-    modelContact->setTable(table);
-
-    for (int i = 0, j = 0; i < modelContact->columnCount(); i++, j++){
-        modelContact->setHeaderData(i, Qt::Horizontal, attributs[j]);
-    }
-    modelContact->setSort(0, Qt::AscendingOrder);
-
-}
-
-void MainWindow::createUI()
-{
-    ui->tableView->setModel(modelContact);
-    ui->tableView->horizontalHeader()->setStretchLastSection(true);
-    modelContact->select();
 }
 
 MainWindow::~MainWindow()
@@ -51,4 +44,37 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+GestionBDD *MainWindow::getDb() const
+{
+    return db;
+}
 
+void MainWindow::setupModel(const QString table, QStringList attributs, QSqlTableModel *(&model)){
+    model = new QSqlTableModel(this);
+    model->setTable(table);
+    for (int i = 0, j = 0; i < model->columnCount(); i++, j++){
+        model->setHeaderData(i, Qt::Horizontal, attributs[j]);
+    }
+}
+
+void MainWindow::createUI()
+{
+    ui->tableViewContact->setModel(modelContact);
+    ui->tableViewContact->setColumnHidden(0, true);
+    ui->tableViewContact->horizontalHeader()->setStretchLastSection(true);
+    ui->tableViewContact->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    ui->tableViewInteraction->setModel(modelInteraction);
+    ui->tableViewInteraction->setColumnHidden(0, true);
+    ui->tableViewInteraction->horizontalHeader()->setStretchLastSection(true);
+    ui->tableViewInteraction->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    ui->tableViewTodo->setModel(modelTodo);
+    ui->tableViewTodo->setColumnHidden(0, true);
+    ui->tableViewTodo->horizontalHeader()->setStretchLastSection(true);
+    ui->tableViewTodo->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    modelContact->select();
+    modelInteraction->select();
+    modelTodo->select();
+}
--- CDAA/gestionnaireContact/mainwindow.h
+++ CDAA/gestionnaireContact/mainwindow.h
@@ -17,12 +17,29 @@ private:
     Ui::MainWindow *ui;
     GestionBDD *db;
     QSqlTableModel *modelContact;
+    QSqlTableModel *modelInteraction;
+    QSqlTableModel *modelTodo;
     // Voir QSqlTableModel pour afficher la bdd dans qt
 
 public:
+    /**
+     * @brief MainWindow le constructeur de MainWindow qui initialize l'ui
+     * @param parent le parent de mainwindow qui est nullptr
+     */
     MainWindow(QWidget *parent = nullptr);
+    /**
+     * Le destructeur de mainwindow qui détruit aussi l'ui
+     */
     ~MainWindow();
-    void setupModel(const QString table, QStringList attributs);
+    /**
+     * @brief setupModel permet paramétrer correctement les QSqlTableModel
+     * @param table La table que l'on veut visualiser
+     * @param attributs Les attributs de la table que l'on veut visualiser
+     */
+    void setupModel(const QString table, QStringList attributs, QSqlTableModel *(&model));
+    /**
+     * @brief createUI création des tables dans la mainwindow
+     */
     void createUI();
     GestionBDD *getDb() const;
 };
