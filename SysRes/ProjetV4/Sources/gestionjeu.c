8041d475c016c786befe4004e607641f871c4843
Daniel <danniel.pinson@gmail.com>
2021-12-16 09:20:13 +0100

    Auto stash before merge of "SysRes" and "origin/SysRes"
    format Makefile

--- SysRes/ProjetV4/Sources/gestionjeu.c
+++ SysRes/ProjetV4/Sources/gestionjeu.c
@@ -1,91 +1,89 @@
 #include "../Header/gestionjeu.h"
 #include <time.h>
 
-gestionnaireJeu* initGestionJeu(){
-    gestionnaireJeu* gj = (gestionnaireJeu*) calloc(1,sizeof(gestionnaireJeu));
-    gj->deck = (int*) calloc(100,sizeof(int));
-    for(int i=0; i<100 ; i++){
-        gj->deck[i] = i+1;
-    }
-    gj->cartePosee = (unsigned int*) calloc(100,sizeof(unsigned int));
-    gj->ordreJeu = (unsigned int*) calloc(80,sizeof(unsigned int));
-    gj->manche = 0;
-    gj->nbManche = 0;
-    gj->nbJoueurs = 0;
-    gj->listeJoueur = (joueur**) calloc(256,sizeof(joueur*));
-    
-    return gj;
+gestionnaireJeu *initGestionJeu() {
+  gestionnaireJeu *gj = (gestionnaireJeu *)calloc(1, sizeof(gestionnaireJeu));
+  gj->deck = (int *)calloc(100, sizeof(int));
+  for (int i = 0; i < 100; i++) {
+    gj->deck[i] = i + 1;
+  }
+  gj->cartePosee = (unsigned int *)calloc(100, sizeof(unsigned int));
+  gj->ordreJeu = (unsigned int *)calloc(80, sizeof(unsigned int));
+  gj->manche = 0;
+  gj->nbManche = 0;
+  gj->nbJoueurs = 0;
+  gj->listeJoueur = (joueur **)calloc(256, sizeof(joueur *));
+
+  return gj;
 }
 
-void AjoutJoueur(gestionnaireJeu* gj, joueur* j){
-    gj->listeJoueur[gj->nbJoueurs] = j;
-    gj->nbJoueurs++;
+void AjoutJoueur(gestionnaireJeu *gj, joueur *j) {
+  gj->listeJoueur[gj->nbJoueurs] = j;
+  gj->nbJoueurs++;
 }
 
-void destroyGestionnaire(gestionnaireJeu* gj){
-    for(int i=0 ; i<gj->nbJoueurs ; i++){
-        destroyJoueur(gj->listeJoueur[i]);
-    }
-    free(gj->listeJoueur);
-    free(gj->cartePosee);
-    free(gj->deck);
-    free(gj->ordreJeu);
-    free(gj);
+void destroyGestionnaire(gestionnaireJeu *gj) {
+  for (int i = 0; i < gj->nbJoueurs; i++) {
+    destroyJoueur(gj->listeJoueur[i]);
+  }
+  free(gj->listeJoueur);
+  free(gj->cartePosee);
+  free(gj->deck);
+  free(gj->ordreJeu);
+  free(gj);
 }
 
-void distribuer(gestionnaireJeu* gj){
-    srand(time(NULL));
+void distribuer(gestionnaireJeu *gj) {
+  srand(time(NULL));
 
-    int cartedistrib[gj->nbJoueurs*gj->manche];
+  int cartedistrib[gj->nbJoueurs * gj->manche];
 
-    for(int jou=0 ; jou<gj->nbJoueurs; jou++){
-        gj->listeJoueur[jou]->cartesEnMain = (int*) calloc(gj->manche,sizeof(int));
+  for (int jou = 0; jou < gj->nbJoueurs; jou++) {
+    gj->listeJoueur[jou]->cartesEnMain = (int *)calloc(gj->manche, sizeof(int));
+  }
+  int k = 0;
+  for (int i = 0; i < gj->manche; i++) {
+    for (int j = 0; j < gj->nbJoueurs; j++) {
+      int cartedistribuee = rand() % 100;
+      while (gj->deck[cartedistribuee] == -1) {
+        cartedistribuee = rand() % 100;
+      }
+
+      ajoutCarteMain(gj->listeJoueur[j], gj->deck[cartedistribuee]);
+
+      cartedistrib[k] = gj->deck[cartedistribuee];
+      gj->ordreJeu[k] = gj->listeJoueur[j]->id;
+      k++;
+
+      gj->deck[cartedistribuee] = -1;
     }
-    int k=0;
-    for(int i=0 ; i<gj->manche ; i++){
-        for(int j=0 ; j<gj->nbJoueurs ; j++){
-            int cartedistribuee = rand() % 100;
-            while (gj->deck[cartedistribuee]==-1)
-            {
-                cartedistribuee = rand() % 100;
-            }
-
-            ajoutCarteMain(gj->listeJoueur[j], gj->deck[cartedistribuee]);
-
-            cartedistrib[k] = gj->deck[cartedistribuee];
-            gj->ordreJeu[k] = gj->listeJoueur[j]->id;
-            k++;
-
-            gj->deck[cartedistribuee] = -1;
-            
-        }
+  }
+  for (int i = 0; i < (gj->nbJoueurs * gj->manche) - 1; i++) {
+    for (int j = i + 1; j < (gj->nbJoueurs * gj->manche); j++) {
+      if (cartedistrib[i] > cartedistrib[j]) {
+        int c = cartedistrib[i];
+        cartedistrib[i] = cartedistrib[j];
+        cartedistrib[j] = c;
+        int ji = gj->ordreJeu[i];
+        gj->ordreJeu[i] = gj->ordreJeu[j];
+        gj->ordreJeu[j] = ji;
+      }
     }
-    for(int i=0; i<(gj->nbJoueurs*gj->manche)-1; i++){
-        for(int j=i+1; j<(gj->nbJoueurs*gj->manche); j++){
-            if(cartedistrib[i]>cartedistrib[j]){
-                int c = cartedistrib[i];
-                cartedistrib[i] = cartedistrib[j];
-                cartedistrib[j] = c;
-                int ji = gj->ordreJeu[i];
-                gj->ordreJeu[i] = gj->ordreJeu[j];
-                gj->ordreJeu[j] = ji; 
-            }
-        }
-    }
+  }
 }
 
-int jeu(joueur*j, gestionnaireJeu* gj){
-    int i = 0;
-    while(gj->ordreJeu[i]==-1){
-        i++;
-    }
-    if(gj->ordreJeu[i] == j->id){
-        gj->cartePosee[i] = j->cartesEnMain[0];
-        jouer(j);
-        gj->ordreJeu[i] = -1;
-        return 1;
-    }else{
-        printf("%s Vous a fait perdre !\n", j->pseudo);
-        return 0;
-    }
+int jeu(joueur *j, gestionnaireJeu *gj) {
+  int i = 0;
+  while (gj->ordreJeu[i] == -1) {
+    i++;
+  }
+  if (gj->ordreJeu[i] == j->id) {
+    gj->cartePosee[i] = j->cartesEnMain[0];
+    jouer(j);
+    gj->ordreJeu[i] = -1;
+    return 1;
+  } else {
+    printf("%s Vous a fait perdre !\n", j->pseudo);
+    return 0;
+  }
 }
--- SysRes/ProjetV4/Sources/joueur.c
+++ SysRes/ProjetV4/Sources/joueur.c
@@ -1,42 +1,41 @@
 #include "../Header/joueur.h"
 
+joueur *NewJoueur(char *pseu, int isHumain, unsigned int i) {
+  joueur *j = (joueur *)calloc(1, sizeof(joueur));
+  j->pseudo = pseu;
+  j->nbCartes = 0;
+  j->id = i;
+  j->humain = isHumain;
 
-joueur* NewJoueur(char* pseu, int isHumain, unsigned int i){
-    joueur* j = (joueur*) calloc(1,sizeof(joueur));
-    j->pseudo = pseu;
-    j->nbCartes = 0;
-    j->id = i;
-    j->humain = isHumain;
-
-    return j;
+  return j;
 }
 
-void destroyJoueur(joueur* j){
-    free(j->cartesEnMain);
-    free(j->pseudo);
-    free(j);
+void destroyJoueur(joueur *j) {
+  free(j->cartesEnMain);
+  free(j->pseudo);
+  free(j);
 }
 
-void ajoutCarteMain(joueur* j, unsigned int carte){
-    j->cartesEnMain[j->nbCartes] = carte;
-    j->nbCartes++;
-    int c;
+void ajoutCarteMain(joueur *j, unsigned int carte) {
+  j->cartesEnMain[j->nbCartes] = carte;
+  j->nbCartes++;
+  int c;
 
-    for(int i=0;i<j->nbCartes-1;i++){
-        for(int k=i+1;k<j->nbCartes;k++){
-            if ( j->cartesEnMain[i] > j->cartesEnMain[k] ) {
-                c = j->cartesEnMain[i];
-                j->cartesEnMain[i] = j->cartesEnMain[k];
-                j->cartesEnMain[k] = c;
-            }
-        }
+  for (int i = 0; i < j->nbCartes - 1; i++) {
+    for (int k = i + 1; k < j->nbCartes; k++) {
+      if (j->cartesEnMain[i] > j->cartesEnMain[k]) {
+        c = j->cartesEnMain[i];
+        j->cartesEnMain[i] = j->cartesEnMain[k];
+        j->cartesEnMain[k] = c;
+      }
     }
+  }
 }
 
-void jouer(joueur* j){
-    for(int i=0; i<j->nbCartes-1;i++){
-        j->cartesEnMain[i] = j->cartesEnMain[i+1];
-    }
-    j->cartesEnMain[j->nbCartes-1] = -1;
-    j->nbCartes--;
+void jouer(joueur *j) {
+  for (int i = 0; i < j->nbCartes - 1; i++) {
+    j->cartesEnMain[i] = j->cartesEnMain[i + 1];
+  }
+  j->cartesEnMain[j->nbCartes - 1] = -1;
+  j->nbCartes--;
 }
