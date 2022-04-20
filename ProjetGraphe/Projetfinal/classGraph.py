17639739a6687cca1885725f30abbecdfdee158b
Yann Mourelon <mourelon.yann@gmail.com>
2022-04-20 19:21:32 +0200

    Code Projet finis
    Ile ne reste plus que le rapport a finir

---
+++ ProjetGraphe/Projetfinal/classGraph.py
@@ -0,0 +1,94 @@
+from pickle import TRUE
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = []
+        self.edges = []
+        self.position = []
+        self.couleur = []
+        self.degree = []
+
+    def add_node(self, n):
+        if n not in self.nodes:
+            self.nodes.append(n)
+            self.position.append([n, [0,0]])
+            self.couleur.append([n, 0])
+            self.degree.append([n, 0])
+            self.degree.sort()
+            self.couleur.sort()
+            self.position.sort()
+            self.nodes.sort()
+
+    def add_edge(self, n1, n2):
+        if n1 in self.nodes and n2 in self.nodes:
+            if (n1, n2) not in self.edges and (n2, n1) not in self.edges:
+                if n1 <= n2:
+                    self.edges.append((n1, n2))
+                else:
+                    self.edges.append((n2, n1))
+                for i in self.degree:
+                    if i[0] == n1:
+                        i[1] += 1
+                    if i[0] == n2:
+                        i[1] += 1
+            self.edges.sort()
+        else:
+            print(n1, " ou/et ", n2, " n\'est pas un noeud.")
+
+    def get_edges(self, n):
+        
+        res = [i for i in self.edges if i[0] == n or i[1] == n]
+        return res
+
+    def get_position(self, n):
+
+        pos = []
+        for i in self.position:
+            if i[0] == n:
+                pos.append(i[1][0])
+                pos.append(i[1][1])
+        return pos
+
+    def modif_position(self, n, pos):
+        for i in self.position:
+            if i[0] == n:
+                i[1][0] = pos[0]
+                i[1][1] = pos[1]
+
+    def modif_couleur(self, n, coul):
+        for i in self.couleur:
+            if i[0] == n:
+                i[1] = coul
+
+    def remove_node(self, n):
+
+        self.nodes.remove(n) #Suppression du noeud dans la liste de noeud
+
+        #Suppression de toutes les arêtes en lien avec le noeud
+        for i in self.edges:
+            if i[0] == n:
+                del self.edges[self.edges.index(i)]
+                for j in self.degree:
+                    if j[0] == i[1]:
+                        j[1] -= 1
+            elif i[1] == n:
+                del self.edges[self.edges.index(i)]
+                for j in self.degree:
+                    if j[0] == i[0]:
+                        j[1] -= 1
+
+        #Suppression de la position du noeud
+        for i in self.position:
+            if i[0] == n:
+                del self.position[self.position.index(i)]
+
+        #Suppression du degré du noeud
+        for i in self.degree:
+            if i[0] == n:
+                del self.degree[self.degree.index(i)]
+
+        #Suppression de la couleur du noeud
+        for i in self.couleur:
+            if i[0] == n:
+                del self.couleur[self.couleur.index(i)]
--- ProjetGraphe/Projetfinal/graph.py
+++ ProjetGraphe/Projetfinal/graph.py
@@ -1,11 +1,12 @@
-import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import copy
+from classGraph import Graph
 
 def genereGraphFromSnap():
     #Création d'un graphe vide grace à networkx
-    G = nx.Graph()
+    G = Graph()
     #on ouvre le fichier snap
     with open(sys.argv[1], "r") as graph_file:
         max = 0 #Plus grand noeud présent dans le graphe (ex : si max=3 alors les noeuds sont : {0,1,2,3})
@@ -22,7 +23,8 @@ def genereGraphFromSnap():
 
         #Création du graphe en ajoutant tout les noeuds de 0 à max
         graph = [i for i in range(max+1)]
-        G.add_nodes_from(graph)
+        for i in graph:
+            G.add_node(i)
         
         #On reparcours le fichier pour ajouter les arêtes
         graph_file.seek(0)
@@ -34,9 +36,15 @@ def genereGraphFromSnap():
     return G
 
 def dessinerGraphe(G, centres):
-    pos = nx.get_node_attributes(G,'pos')
+    X, Y, size = [G.position[i][1][0] for i in G.nodes], [G.position[i][1][1] for i in G.nodes], [100 for i in G.nodes]
 
-    nx.draw(G, pos, with_labels=True, font_size=12,font_weight='bold')
+    plt.scatter(X,Y)
+
+    for i in G.nodes:
+        plt.text(X[i],Y[i],str(i))
+
+    for i in G.edges:
+        plt.plot([X[i[0]], X[i[1]]], [Y[i[0]], Y[i[1]]], color='blue')
 
     ax = plt.gca()
 
@@ -47,20 +55,24 @@ def dessinerGraphe(G, centres):
 
     plt.axis("equal")
 
-    #Affichage direct du graphe
-    #plt.show()
     #Enregistrement du graphe au format PDF
-    #plt.savefig("path.pdf")
+    print("Voulez-vous enregistrer au format PDF ? [Y/N]")
+    rep = input()
+    if rep == 'Y':
+        plt.savefig("graph.pdf")
+    #Affichage direct du graphe
+    plt.show()
+    
 
-def degenerecence(G):
+def degenerecence(G2):
     degen = 1 #Initialisation de la dégénérescence à 1 (la dégénéréscence minimal)
     degSupDegen = False #Savoir quand sortir d'une boucle
-    G2 = nx.Graph(G) #Copie du graphe pour travailler plus simplement dessus
-    centres = [-1 for i in G.nodes()] #Initialisation du tableau de centres à -1
+    
+    centres = [-1 for i in G2.nodes] #Initialisation du tableau de centres à -1
     while(True):
         while(degSupDegen == False):
             index = []
-            for i in G2.degree():
+            for i in G2.degree:
                 if i[1] <= degen:
                     index.append(i[0]) #Mise en mémoire du noeuds de degré inférieur ou égal à degen
                     centres[i[0]] = degen #Mise à jour du tableau de centres du graphe G
@@ -71,7 +83,7 @@ def degenerecence(G):
                 degSupDegen = True #Si tout les degrés du graphe sont supérieur à la dégénéréscence, on passe à true
 
         degSupDegen=False
-        if len(G2.degree()) == 0:
+        if len(G2.degree) == 0:
             return degen, centres #Retourne le tableau de centres ainsi que la dégénéréscence du graphe lorsque tout les noeuds ont étés supprimés de G2
         else :
             degen+=1
@@ -79,17 +91,17 @@ def degenerecence(G):
 def modifPos(G, degenGraph, centres):
     #NetworkX propose déjà une méthode pour afficher en cercle le graphe, mais on le refais ici
 
-    nbnoeuds = max(G.nodes())
-    for i in G.nodes():
+    nbnoeuds = max(G.nodes)
+    for i in G.nodes:
         rayon = (degenGraph-centres[i]+1)
         alpha1 = (i*np.pi)/((centres.count(max(centres))+1)/2)
         alpha2 = (i*np.pi)/((nbnoeuds+1)/2)
         #On fait un cercle avec les noeuds qui ont le plus grand centre
         if centres[i] == max(centres):
-            G.nodes[i]['pos'] = (rayon*np.cos(alpha1), rayon*np.sin(alpha1))
+            G.modif_position(i, [rayon*np.cos(alpha1), rayon*np.sin(alpha1)])
         # on fait un autre cercle avec tout les autres noeuds et de leurs centre
         else:
-            G.nodes[i]['pos'] = (rayon*np.cos(alpha2), rayon*np.sin(alpha2))
+            G.modif_position(i, [rayon*np.cos(alpha2), rayon*np.sin(alpha2)])
 
 def sortNodes(centres):
     #Création d'une liste intermédiaire qui va contenir des listes contenant le sommet qui ont ce centre
@@ -100,18 +112,22 @@ def sortNodes(centres):
         # On met dans la liste des centres i (indexée à i-1), le sommet k
         inter[i-1].append(k)
         k += 1
-    return sorted([item for sublist in inter for item in sublist], reverse=True)
+    
+    return list(reversed([item for sublist in inter for item in list(reversed(sublist))]))
 
 def colorGlouton(G, centres):
 
-    color = [-1 for i in G.nodes()]
+    color = [-1 for i in G.nodes]
 
     #On parcourt tous les noeuds de notre graphe suivant le numéro de centre décroissant
     for i in sortNodes(centres):
         #initialisation de la couleur à "0"
         couleur = 0
         #récupération des couleurs des noeuds voisins
-        couleurvoisins = [color[edge[1]] for edge in G.edges(i)]
+        couleurvoisins = []
+        for edge in G.get_edges(i):
+            k = 1 if edge[0]==i else 0
+            couleurvoisins.append(color[edge[k]])
         couleurvoisins.sort() #tri dans l'ordre croissant
         for j in range(len(couleurvoisins)):
             if couleurvoisins[j] == couleur:
@@ -119,29 +135,28 @@ def colorGlouton(G, centres):
 
         #affectation de la couleur "minimale" dans le tableau de couleur
         color[i] = couleur
-        G.nodes[i]['color'] = 'r'
     return color
 
 def TestGraphe():
     G = genereGraphFromSnap()
 
-    print(G)
+    print(f"{G.nodes = }")
 
     #Calcul de la dégénérécence du graphe G
-    degenGraph, centres = degenerecence(G)
-    print("degenerecence = ", degenGraph)
-
+    degenGraph, centres = degenerecence(copy.deepcopy(G))
+    
     #Calcul de la coloration du graphe G
     color = colorGlouton(G, centres)
-    print("nb Couleur utilisés : ", max(color)+1)
-
-    print("Graph =", G.nodes())
+    
     print(f"{centres = }")
     print(f"{color = }")
+    print("degenerecence = ", degenGraph)
+    print("nb Couleur utilisés : ", max(color)+1)
+    
 
     modifPos(G, degenGraph, centres)
 
     dessinerGraphe(G, centres)
-
+    
 
 TestGraphe()
